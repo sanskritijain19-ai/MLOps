38476108b64b35501fe977d3c85754934ce6556a
sanskritijain19-ai <sanskritijain19@gmail.com>
2026-04-05 13:56:21 +0000

    7th commit

--- mlops/hosting/hosting.py
+++ mlops/hosting/hosting.py
@@ -1,7 +1,6 @@
 from huggingface_hub import HfApi
 import os
 
-
 api = HfApi(token=os.getenv("HF_TOKEN"))
 api.upload_folder(
     folder_path="mlops/deployment",     # the local folder containing your files
